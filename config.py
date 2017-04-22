6fdb2e0a7f2947ded61c9170d587df31463b8b24
Steve Blamey <steve.blamey@adbury.net>
2017-04-22 15:35:09 +0100

    Add config options

--- config.py
+++ config.py
@@ -5,7 +5,14 @@ APP_HOST = 'https://demo.uzerp.com'
 APP_USERNAME = 'admin'
 APP_PASSWORD = 'admin'
 
+MIN_WAIT = 5000
+MAX_WAIT = 15000
+
 # Collect the response time for each request and save to a CSV file
 COLLECT_STATS = True
+
+# Plot a graph of the collected stats using gnuplot
+CHART_STATS = False
+
 # Filename for the csv
 STATS_FILE = 'stats.csv'
