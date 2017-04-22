2f38e33cb8fad0bce8895d2460bde8b2f4e9bf18
Steve Blamey <steve.blamey@adbury.net>
2017-04-22 15:36:07 +0100

    Refactor to load tasks from the uztasks module, improve stats and add charting option via gnuplot

--- locustfile.py
+++ locustfile.py
@@ -1,28 +1,55 @@
 import csv
+from datetime import datetime
+from subprocess import call
 
 from bs4 import BeautifulSoup
+import numpy as np
 
 from locust import events
 from locust import HttpLocust
 from locust import TaskSet
-from locust import task
+from locust.log import console_logger
 
 import config
+from uztasks import tasks
 
 
 # Hook into some events to collect statistics
 def collect_stats(request_type, name, response_time, response_length):
-    stats.append([request_type, name, response_time])
+    stats.append(
+        [datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3],
+        request_type,
+        name,
+        response_time])
 
 
 def write_stats(filename=config.STATS_FILE):
-    """Write collected response times to a CSV file."""
+    """
+    Log some info and write collected response times to a CSV file.
+    """
+    response_times = [r[3] for r in stats]
+    std_dev = 3 * np.std(response_times)
+    mean = np.mean(response_times)
+    outliers = [r for r in stats if abs(r[3] - mean) > std_dev]
+
+    console_logger.info("\n uzERP Performance Summary:")
+    console_logger.info('3 x Std. Dev: {}, with {} Outliers ({:.2f}%)'.format(
+        std_dev,
+        len(outliers),
+        (float(len(outliers)) / len(response_times) * 100)))
+    console_logger.info('Min request time: {}'.format(np.amin(response_times)))
+    console_logger.info('Max request time: {}'.format(np.amax(response_times)))
+
     csv_file = open(filename, 'w')
     csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_MINIMAL)
-    stats.insert(0, ['Request Type', 'URL', 'Response Time'])
+    stats.insert(0, ['Time', 'Request Type', 'URL', 'Response Time'])
     for stat in stats:
         csv_writer.writerow(stat)
     csv_file.close()
+
+
+def chart_stats():
+    call('gnuplot -c utils/gnuplot/chart.plt -p', shell=True)
 
 
 if config.COLLECT_STATS:
@@ -30,41 +57,14 @@ if config.COLLECT_STATS:
     events.request_success += collect_stats
     events.quitting += write_stats
 
+if config.CHART_STATS:
+    events.quitting += chart_stats
 
-class WebsiteTasks(TaskSet):
-    def request_resources(self, response=None):
-        """
-        Parse HTML response content to extract resource urls (javascript
-        and css) and request those resources from the host.
 
-        Args:
-            response (object): locust request response
-        """
-        soup = BeautifulSoup(response.content, 'html.parser')
-        resource_urls = set()
-
-        # Find resource urls with a src attribute
-        for res in soup.find_all(src=True):
-            url = res['src']
-            if url[0] != '/':
-                url = '/' + url
-            resource_urls.add(url)
-
-        # Find stylesheet urls
-        for rel in soup.find_all(rel='stylesheet'):
-            url = rel['href']
-            resource_urls.add(url)
-
-        # Find module js url
-        js_url_start = response.content.find('loadScript("') + 12
-        js_url_end = response.content.find('"', js_url_start)
-        module_js = '/' + response.content[js_url_start:js_url_end]
-        module_js.replace(self.parent.host, '')
-        resource_urls.add(module_js)
-
-        # Request the page resources
-        for url in resource_urls:
-            self.client.get(url)
+class UserBehaviour(TaskSet):
+    """Primary TaskSet, all other tasks are nested within."""
+    tasks = {tasks.SalesTasks: 1,
+             tasks.PurchasingTasks: 1}
 
     def on_start(self):
         """Log-in to the application."""
@@ -79,16 +79,9 @@ class WebsiteTasks(TaskSet):
             'rememberUser': 'true',
         })
 
-    @task
-    def list_sales_orders(self):
-        """Request the sales order listing."""
-        response = self.client.get(
-            '/?controller=sorders&module=sales_order&action=index')
-        self.request_resources(response)
-
 
 class WebsiteUser(HttpLocust):
     host = config.APP_HOST
-    task_set = WebsiteTasks
-    min_wait = 5000
-    max_wait = 15000
+    min_wait = config.MIN_WAIT
+    max_wait = config.MAX_WAIT
+    task_set = UserBehaviour
